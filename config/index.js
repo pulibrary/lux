227867a14849756c4f3e4d35aa31d70bea63566e
Shaun Ellis <shaun@sdellis.com>
2018-05-02 15:17:03 -0400

    fixes assets path

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
     assetsSubDirectory: "assets",
-    assetsPublicPath: "/",
+    assetsPublicPath: "/lux/",
 
     // Source map
     productionSourceMap: false,
