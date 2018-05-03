275b2df402db2b8378247fe216600c01d5e22220
Shaun Ellis <shaun@sdellis.com>
2018-05-03 10:34:49 -0400

    npm setup

--- config/index.js
+++ config/index.js
@@ -32,10 +32,10 @@ module.exports = {
   },
   system: {
     // Template for index.html
-    index: path.resolve(__dirname, "../dist/index.html"),
+    index: path.resolve(__dirname, "../lib/index.html"),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, "../dist"),
+    assetsRoot: path.resolve(__dirname, "../lib"),
     assetsSubDirectory: "system",
     assetsPublicPath: "/",
 
