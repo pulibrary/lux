ce549db5db1f0a47970a6f77e91d37c62db3bea7
Axa Mei Liauw <aliauw@princeton.edu>
2018-06-15 13:24:08 -0400

    Adds element components as per University guidelines. Closes #9

---
+++ src/elements/_UniversityCopyright.vue
@@ -0,0 +1,45 @@
+<template>
+  <component :is="type" class="copyright">
+    &copy; {{ new Date().getFullYear() }} The Trustees of Princeton University
+  </component>
+</template>
+
+<script>
+/**
+ * Used to show the University’s copyright notice, including the current year
+ * (“© 20xx The Trustees of Princeton University”) in the footer.
+ */
+export default {
+  name: "UniversityCopyright",
+  status: "review",
+  release: "1.0.0",
+  type: "Element",
+  props: {
+    /**
+     * The html element name used for the wrapper.
+     */
+    type: {
+      type: String,
+      default: "",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.copyright {
+  @include reset;
+  @include stack-space($space-base);
+  font-family: $font-family-heading;
+  font-size: $font-size-x-small;
+  line-height: $line-height-heading;
+  color: $color-rich-black;
+}
+</style>
+
+
+<docs>
+  ```jsx
+  <university-copyright type="span"></university-copyright>
+  ```
+</docs>
