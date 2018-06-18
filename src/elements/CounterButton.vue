d9d0e55c1f91ef26d67203d13f0f2e8a29a42b53
Shaun Ellis <shaun@sdellis.com>
2018-06-18 13:42:40 -0400

    vuex now working in docs and system

---
+++ src/elements/CounterButton.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="CounterButton">
+	<button class="button"
+		@click.prevent="increment">
+		0
+	</button>
+	<span>{{message}}</span>
+</div>
+</template>
+
+<script>
+//import { mapGetters } from "vuex";
+
+/**
+ * Button that counts how many times it was pressed and exposes a `@public` method to reset itself.
+ */
+export default {
+  name: "CounterButton",
+  data: function() {
+    return {
+      message: "hello!",
+    }
+  },
+  computed: {
+    // ...mapGetters(["count"]),
+    here() {
+      return this.$store.getters.here
+    },
+  },
+  methods: {
+    /**
+     * Increments the counter. This method is not marked @public and is not visible in the styleguide.
+     */
+    increment() {
+      console.log(this.$store.getters.here)
+      // this.$store.commit('increment')
+      // /**
+      //  * After increment event
+      //  * @event after
+      //  * @type {number}
+      //  */
+      // this.$emit("after", this.value);
+    },
+  },
+}
+</script>
+<docs>
+Don't forget that you can debug it with [vue-devtools](https://github.com/vuejs/vue-devtools)
+
+```js
+    <CounterButton/>
+```
+</docs>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // Vue Design System: The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue"
+import Vuex from "vuex"
 import App from "@/App"
 import router from "@/router"
 import WebFontLoader from "@/utils/webFontLoader" // eslint-disable-line no-unused-vars
@@ -11,13 +12,27 @@ import DesignSystem from "@/system"
 
 Vue.use(DesignSystem)
 Vue.use(Meta)
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    here: "test",
+  },
+  getters: {
+    here(state) {
+      return state.here
+    },
+  },
+})
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
   router,
+  store,
   template: "<App/>",
   components: { App },
 })
