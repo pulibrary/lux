f2ca77cb9d2c6a915a644b51df33327befe5fc40
Axa Mei Liauw <aliauw@princeton.edu>
2018-06-29 15:41:30 -0400

    Adds media-image and card components

---
+++ src/elements/Card.vue
@@ -0,0 +1,121 @@
+<template>
+  <article class="card" :class="[size, { 'card-selected': selected }, { 'card-edited': edited }, { 'card-disabled': disabled }]">
+    <slot/>
+  </article>
+</template>
+
+<script>
+/**
+ * Cards are used to apply a container around a related grouping of information.
+ */
+export default {
+  name: "Card",
+  status: "prototype",
+  release: "1.0.0",
+  type: "Element",
+  props: {
+    /**
+     * Sets the URL linking to the card content
+     */
+    cardUrl: {
+      type: String,
+      default: "",
+    },
+    /**
+     * Sets the size of the card `small, medium, large`
+     */
+    size: {
+      type: String,
+      default: "medium",
+      validator: value => {
+        return value.match(/(small|medium|large)/)
+      },
+    },
+    /**
+     * Indicates whether the card is selected.
+     */
+    selected: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Indicates whether the object a card represents has been altered from its persisted form.
+     */
+    edited: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Indicates the user cannot interact with the card.
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Indicates the user cannot interact with the card.
+     */
+    cardTitle: {
+      type: String,
+      default: "",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  @include reset;
+  @include stack-space($space-base);
+  font-family: $font-family-text;
+  line-height: $line-height-base;
+  background: $color-white;
+  box-shadow: $box-shadow-small;
+  color: $color-rich-black;
+
+  &:focus,
+  &-selected {
+    box-shadow: $box-shadow-selected;
+  }
+
+  &-edited {
+    background: $color-princeton-orange-on-white;
+  }
+
+  &-disabled {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+
+  &.small {
+    width: 200px;
+  }
+
+  &.medium {
+    width: 300px;
+  }
+
+  &.large {
+    width: 400px;
+  }
+
+  .heading,
+  .text-style {
+    padding: 0 1rem;
+  }
+
+  .text-style {
+    padding-bottom: 1rem;
+  }
+}
+</style>
+
+<docs>
+  ```jsx
+  <card size="medium">
+    <media-image src="https://picsum.photos/600/300/?random" height="medium"></media-image>
+    <heading level="h2">Title</heading>
+    <text-style variation="default">Design isn’t just about the look and feel. Design is how it works.</text-style>
+  </card>
+  ```
+</docs>
