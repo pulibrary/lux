6deec0910d8fd5fbebe1b973372213a3d928418b
Axa Mei Liauw <aliauw@princeton.edu>
2018-07-09 14:51:47 -0400

    Adds library header pattern

---
+++ src/elements/_LibraryLogo.vue
@@ -0,0 +1,44 @@
+<template>
+  <component :is="type" class="library-logo">
+    <a href="https://library.princeton.edu"><svg-icon name="library-logo" ariaLabel="Princeton University Library" width="155px" height="34px"></svg-icon></a>
+  </component>
+</template>
+
+<script>
+/**
+ * Used to identify that the site is a Princeton University site in the footer
+ * and links to princeton.edu.
+ */
+export default {
+  name: "LibraryLogo",
+  status: "review",
+  release: "1.0.0",
+  type: "Element",
+  props: {
+    /**
+     * The html element name used for the wrapper.
+     */
+    type: {
+      type: String,
+      default: "div",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.library-logo {
+  @include reset;
+  font-family: $font-family-heading;
+  font-size: $font-size-x-small;
+  line-height: $line-height-heading;
+  color: $color-rich-black;
+}
+</style>
+
+
+<docs>
+  ```jsx
+  <library-logo></library-logo>
+  ```
+</docs>
---
+++ src/patterns/LibraryHeader.vue
@@ -0,0 +1,99 @@
+<template>
+  <component :is="type" :class="['library-header ', variation]">
+    <library-logo></library-logo>
+    <a class="app-name" href="/">{{appName}}</a>
+    <spacer></spacer>
+    <slot/>
+  </component>
+</template>
+
+<script>
+/**
+ * “Example Component” is used to visually communicate core parts of the product and available actions.
+ */
+export default {
+  name: "LibraryHeader",
+  status: "prototype",
+  release: "1.0.0",
+  props: {
+    /**
+     * The html element name used for the container
+     */
+    type: {
+      type: String,
+      default: "div",
+    },
+    /**
+     * The name of the application or site
+     */
+    appName: {
+      type: String,
+      default: "",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+/**
+ * Styles in a top-level App component and in layout components may be global,
+ * but all other components should always be scoped (using either scoped
+ * attribute or class based scoping).
+ */
+.library-header {
+  @include reset;
+  @include stack-space($space-small);
+  font-family: $font-family-heading;
+  background: $color-grayscale-darker;
+  color: set-text-color($color-rich-black, $color-white);
+
+  height: $height-footer;
+
+  align-items: center;
+  display: flex;
+  padding: 0 24px;
+
+  border-top: 3px solid $color-princeton-orange-on-black;
+  @media #{$media-query-medium} {
+    @include stack-space($space-base);
+  }
+
+  .app-name {
+    border-left: 1px solid $color-grayscale-light;
+    color: $color-white;
+    font-size: $font-size-x-large;
+    font-weight: 400;
+    height: 35px;
+    letter-spacing: 1px;
+    line-height: 33px;
+    margin-left: 1rem;
+    padding-left: 1rem;
+    text-decoration: none;
+  }
+
+  .nav {
+    width: auto;
+  }
+
+  .nav /deep/ a {
+    color: $color-white;
+
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
+
+<docs>
+  ```jsx
+  <library-header app-name="Catalog">
+  <menu-bar type="nav" :menuItems="[
+      {name: 'Help', component: 'Help', href: '/help/'},
+      {name: 'Feedback', component: 'Feedback', href: '/feedback/'},
+      {name: 'Your Account', component: 'Account', href: '/account/'}
+    ]"/>
+  </library-header>
+  ```
+</docs>
