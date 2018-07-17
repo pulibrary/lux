5b2e4abbf0b4bd668449280d0ca6f5f42dc6942f
Shaun Ellis <shaun@sdellis.com>
2018-07-17 00:01:52 -0400

    filesetForm now uses input components

--- src/patterns/OrderManager.vue
+++ src/patterns/OrderManager.vue
@@ -154,6 +154,7 @@ export default {
       "id": "aea40813-e0ed-4307-aae9-aec53b26bdda",
       "label": "Resource with 3 files",
       "viewingHint": "individuals",
+      "startPage": "8ffd7a03-ec0e-46c1-a347-e4b19cb7839f",
       "__typename": "ScannedResource",
       "members": [
         {
