519cdeff7ed8b00664c660ccc62c98fde1be481d
Shaun Ellis <shaun@sdellis.com>
2018-08-28 15:37:48 -0400

    tries to get cypress tests to run on circle

--- config/index.js
+++ config/index.js
@@ -92,4 +92,37 @@ module.exports = {
     cssSourceMap: true,
     vuex: "../src/store/index",
   },
+  test: {
+    // Paths
+    assetsSubDirectory: "assets",
+    assetsPublicPath: "/",
+    proxyTable: {},
+
+    // Various Dev Server settings
+    host: "localhost", // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: false, // Show OS X notification?
+    poll: false, // https://webpack.js.org/configuration/watch/#watchoptions
+
+    // If true, your code will be linted during bundling and
+    // linting errors and warnings will be shown in the console.
+    useEslint: false,
+    // If true, eslint errors and warnings will also be shown in the error overlay
+    // in the browser.
+    showEslintErrorsInOverlay: false,
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: "cheap-module-eval-source-map",
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
+
+    // Source map
+    cssSourceMap: true,
+    vuex: "../src/store/index",
+  },
 }
