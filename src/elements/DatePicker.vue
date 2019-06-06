fe366f193af1b30fdb25dfeb595a602c24d89fc6
Shaun Ellis <shaun@sdellis.com>
2019-06-06 16:11:09 -0400

    WIP - need to debug popover defaults and input-text integration

---
+++ src/elements/DatePicker.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-date-picker v-model="date" :popover="popover">
+    <input-text
+      id="startDate"
+      label="Start Date"
+      width="auto"
+      slot-scope="{ inputProps, inputEvents }"
+      v-bind="inputProps"
+      v-on="inputEvents"
+    >
+      <button><logo-twitter /></button>
+    </input-text>
+  </v-date-picker>
+</template>
+
+<script>
+/**
+ * Form Inputs are used to allow users to provide text input when the expected
+ * input is short. Form Input has a range of options and supports several text
+ * formats including numbers. For longer input, use the `FormTextarea` element.
+ */
+import Vue from "vue"
+import VCalendar from "v-calendar"
+import "v-calendar/lib/v-calendar.min.css"
+Vue.use(VCalendar)
+
+export default {
+  name: "DatePicker",
+  status: "prototype",
+  release: "1.0.0",
+  type: "Element",
+  components: {
+    VCalendar,
+  },
+  data() {
+    return {
+      date: new Date(),
+      popover: {
+        placement: "bottom",
+        visibility: "click",
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
+
+<docs>
+  ```jsx
+  <div>
+    <date-picker />
+  </div>
+  ```
+</docs>
