13f225e18eae95139f92eaf3cde38916d24859cb
Trey Pendragon <tpendragon@princeton.edu>
2020-04-15 12:30:40 -0700

    Convert Heading to be a functional component.
    This is much faster, and improves Figgy's order manager.

--- test/unit/specs/components/heading.spec.js
+++ test/unit/specs/components/heading.spec.js
@@ -7,39 +7,43 @@ const localVue = createLocalVue()
 describe("Heading.vue", () => {
   let wrapper
 
-  beforeEach(() => {
-    wrapper = mount(Heading, {
+  function buildWrapper(props) {
+    return mount(Heading, {
       localVue,
       slots: {
         default: "Here's a heading.",
       },
-    })
-  })
+      propsData: props
+    }
+    )
+  }
 
   it("should render the appropriate level heading", () => {
+    let wrapper = buildWrapper({})
     const heading1 = wrapper.find("h1")
     expect(heading1.is("h1")).toBe(true)
-    wrapper.setProps({ level: "h3" })
+    wrapper = buildWrapper({ level: "h3" })
     const heading3 = wrapper.find("h3")
     expect(heading3.is("h3")).toBe(true)
     expect(heading3.text()).toBe("Here's a heading.")
   })
 
   it("should render the appropriate class name", () => {
-    wrapper.setProps({ level: "h3", size: "h2" })
+    let wrapper = buildWrapper({ level: "h3", size: "h2" })
     const heading3 = wrapper.find("h3")
     expect(heading3.is("h3")).toBe(true)
     expect(heading3.is(".h2"))
   })
 
   it("should display the slotted text", () => {
-    wrapper.setProps({ level: "h2" })
+    let wrapper = buildWrapper({ level: "h2" })
     const heading2 = wrapper.find("h2")
     expect(heading2.is("h2")).toBe(true)
     expect(heading2.text()).toBe("Here's a heading.")
   })
 
   it("has the expected html structure", () => {
+    let wrapper = buildWrapper({})
     expect(wrapper.element).toMatchSnapshot()
   })
 })
