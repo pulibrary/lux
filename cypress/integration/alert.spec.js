0e442a676b92aed5fdc9fdfdde8aecbec03ba595
Axa Mei Liauw <aliauw@princeton.edu>
2020-06-23 10:49:49 -0400

    Adds axe

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -3,6 +3,12 @@ describe("Integration test with visual testing", function() {
     // Load the page or perform any other interactions with the app.
     cy.visit("http://localhost:6060/#/Elements/Alert")
 
+    // Inject axe
+    cy.injectAxe()
+    cy.checkA11y({
+      include: ["[class^='rsg--preview']"],
+    })
+
     // Take a snapshot for visual diffing
     cy.percySnapshot()
   })
