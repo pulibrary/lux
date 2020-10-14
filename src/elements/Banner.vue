c6592440aaf759bc15bd4130e01e588369641e7c
Shaun Ellis <shaun@sdellis.com>
2020-10-14 17:31:39 -0400

    Adds Banner component and incomplete tests

---
+++ src/elements/Banner.vue
@@ -0,0 +1,191 @@
+<template>
+  <transition name="fade">
+    <div v-if="show" :class="['lux-banner', { 'lux-banner-fullscreen': isFullScreen }]">
+      <div class="lux-announcement open">
+        <wrapper fullWidth="true">
+          <button
+            v-if="dismissible"
+            type="button"
+            class="lux-announcement__close"
+            data-dismiss="alert"
+            aria-label="Close"
+            @click="hideAlert()"
+          >
+            <span aria-hidden="true">×</span>
+          </button>
+          <slot>{{ bannerMessage }}</slot>
+        </wrapper>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+/**
+ * Banners are used to provide timely, general information to users of a website or app.
+ */
+export default {
+  name: "Banner",
+  status: "prototype",
+  release: "2.16.3",
+  type: "Element",
+  metaInfo: {
+    title: "Global alert message to user.",
+    htmlAttrs: {
+      lang: "en",
+    },
+  },
+  data: function() {
+    return {
+      bannerMessage: "This is the announcement description.",
+      show: true,
+    }
+  },
+  computed: {
+    isFullScreen() {
+      return this.fullscreen === "true" ? true : false
+    },
+  },
+  props: {
+    /**
+     * Allows the banner to act as an overlay, near the top of the screen.
+     */
+    fullscreen: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * User can manually hide the notification.
+     */
+    dismissible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    hideBanner() {
+      this.show = false
+    },
+  },
+  mounted() {
+    if (this.autoclear) {
+      setTimeout(() => {
+        this.show = false
+      }, 2000)
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.lux-banner-fullscreen {
+  position: fixed;
+  top: 5vh;
+  left: 50%; /* move the left edge to the center … */
+  margin-left: -25vw !important; /* … and move it to the left half the box’ width. */
+  z-index: 9999;
+  width: 50vw;
+}
+
+.lux-banner {
+  @include reset;
+  font-family: $font-family-text;
+  // padding: 0.75rem 1.25rem;
+  // margin-bottom: 1rem;
+  // border: 1px solid transparent;
+  // border-radius: 0.25rem;
+  // position: relative;
+}
+
+.lux-banner-dismissible > span {
+  width: calc(100% - 25px);
+  display: inline-block;
+}
+
+.lux-banner-dismissible .lux-banner__close {
+  position: absolute;
+  top: -4px;
+  right: -5px;
+  padding: 0.75rem 1.25rem;
+  color: inherit;
+}
+
+.lux-close {
+  float: right;
+  font-size: 1.5rem;
+  font-weight: 700;
+  line-height: 1;
+  color: #000;
+  text-shadow: 0 1px 0 #fff;
+  opacity: 0.5;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.lux-announcement {
+  position: relative;
+  background: #1da3ff;
+  color: white;
+  padding: 0.5rem;
+  transition: 0.2s;
+  z-index: 1;
+
+  a {
+    color: white;
+    transition: 0.25s ease;
+
+    &:hover {
+      opacity: 0.75;
+    }
+  }
+
+  button.lux-announcement__close {
+    padding: 0;
+    cursor: pointer;
+    background: 0 0;
+    border: 0;
+    -webkit-appearance: none;
+  }
+
+  &__close {
+    position: absolute;
+    right: 15px;
+    top: 15px;
+    font-size: 18px;
+    width: 25px;
+    height: 25px;
+    font-weight: 600;
+    text-align: center;
+    cursor: pointer;
+    color: white;
+    opacity: 0.65;
+    transition: 0.25s ease;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+
+.open {
+  top: 0;
+}
+</style>
+
+<docs>
+  ```jsx
+  <div>
+    <banner dismissible>
+      <h2>Fall Semester Announcements</h2>
+      <p>We’re currently in beta for students and will be introducing faculty, advisor and staff functionality in the coming months.</p>
+    </banner>
+  </div>
+  ```
+</docs>
---
+++ test/unit/specs/components/banner.spec.js
@@ -0,0 +1,51 @@
+import { createLocalVue, mount } from "@vue/test-utils"
+import Banner from "@/elements/Banner.vue"
+
+// create an extended `Vue` constructor
+const localVue = createLocalVue()
+
+describe("Banner.vue", () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(Banner, {
+      localVue,
+      slots: {
+        default: "Here's some info for you.",
+      },
+      // if props are set here, buttons aren't rendered
+      // props: {
+      //   dismissible: true,
+      // }
+    })
+  })
+
+  it("should have an alert message", () => {
+    const el = wrapper.find(".lux-banner")
+    expect(el.text()).toBe("Here's some info for you.")
+  })
+
+  it("should have a button when dismissible", () => {
+    wrapper.setProps({ dismissible: true })
+    const button = wrapper.find("button")
+    expect(wrapper.vm.dismissible).toBe(true)
+    expect(button.is("button")).toBe(true)
+  })
+
+  it("should be dismissible on click", () => {
+    wrapper.setProps({ dismissible: true })
+    const button = wrapper.find("button")
+    button.trigger("click")
+    expect(wrapper.isEmpty()).toBe(true)
+  })
+
+  it("should hide banner when hideBanner() is called", () => {
+    expect(wrapper.vm.show).toBe(true)
+    wrapper.vm.hideBanner()
+    expect(wrapper.vm.show).toBe(false)
+  })
+
+  it("has the expected html structure", () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
